d134b09c5fd65e0d10fe8aa18a38973a22c9bf42
João <jjvcamposs@gmail.com>
2018-06-15 16:28:28 -0300

    add server side

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
+var PORT = 1000;
 
-app.listen(8000, ()=> {
-    console.log("Ouvindo na porta 8000 !!");
+app.listen(PORT, ()=> {
+    console.log(`Ouvindo na porta ${PORT} =)`);
 })
