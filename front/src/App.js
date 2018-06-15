1bd24b7614602e5c6d1a1d75ed7fed8d7c5d4b0a
João <jjvcamposs@gmail.com>
2018-06-15 18:39:13 -0300

    consumindo api e mostrando

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,35 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      mensagem: ''
+    };
+  }
+
+  componentDidMount() {
+    this.helloWord()
+      .then(res => this.setState({ mensagem: res.msg})) // Se tudo estiver ok , altera o estado
+      .catch(err => console.log(err)); // Se não, mostra erro :)
+  }
+
+  helloWord = async() => {
+    const response = await fetch('/api/helloword') // chamo a rota
+    const body = await response.json(); // recupero o body
+    if(response.status !== 200) throw Error(body.message); // verifico o status da api
+
+    return body; // retorno para montar o componente
+  }
+
   render() {
     return (
       <div className="App">
-        
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">{this.state.mensagem}</p>
       </div>
     );
   }
--- server/app.js
+++ server/app.js
@@ -1,6 +1,18 @@
-var express = require('express');
-var app = express();
-var PORT = 8000;
+'use strict'
+
+var express = require('express'); // Sistema de Rotas
+var bodyParser = require('body-parser'); // BodyParser
+var app = express(); // Aplica as rotas ao meu app
+var PORT = 8000; // Porta da minha aplicação
+
+// Configurando BodyParser
+app.use(bodyParser.json()); // Dizendo que irei enviar os dados via json
+app.use(bodyParser.urlencoded({extended: true})); // Os dados serão recuperados com method POST
+
+
+app.get('/api/helloword', (req, res) => {
+    res.send({ msg: "hello word de minha api !! ;)"});
+});
 
 // Iniciando aplicação
 app.listen(PORT, () => {
