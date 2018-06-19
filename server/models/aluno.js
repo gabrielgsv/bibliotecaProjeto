66fb1830f33ccd60b03d936c81f7a1f91605eac1
João <jjvcamposs@gmail.com>
2018-06-19 15:49:33 -0300

    adicionando models

--- server/models/aluno.js
+++ server/models/aluno.js
@@ -1,20 +1,20 @@
 const db = require('../config/db.js'); // recupera conexão com o banco
 const Sequelize = require('sequelize'); // instancia sequelize
 
- const Aluno = db.define('aluno',{
+ const Aluno = db.define('aluno'/*nome da tabela*/,{
     id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
+        type: Sequelize.INTEGER, // tipo
+        primaryKey: true, 
         autoIncrement: true 
     },
     nome:{
         type: Sequelize.STRING,
-        allowNull: false,
+        allowNull: false, // não pode valores nuloes
     },
     email:{
         type: Sequelize.STRING,
         allowNull: false,
-        unique: true,
+        unique: true, // não pode valores repetidos
     },
     senha:{
         type: Sequelize.DOUBLE,
